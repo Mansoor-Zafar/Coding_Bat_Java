0729b4d549eb71c7becd45b362305a3fa26bb9c3
Mansoor-Zafar <145301137+Mansoor-Zafar@users.noreply.github.com>
2023-10-29 21:58:49 -0400

    Create stringSplosion.java

---
+++ Warmup-2/stringSplosion.java
@@ -0,0 +1,6 @@
+public String stringSplosion(String str) {
+  StringBuilder result = new StringBuilder();
+  for(int i = 0; i < str.length(); ++i)
+    result.append(str.substring(0,i+1));
+  return result.toString();
+}
